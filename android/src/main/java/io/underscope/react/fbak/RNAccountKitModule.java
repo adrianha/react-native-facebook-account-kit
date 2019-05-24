2c5f045db2b47c18759dcdbd06fd88975804155d
Adrian Hartanto <s.adrianh@gmail.com>
2019-05-24 15:18:11 +0700

    Remove receiveSMS method

--- android/src/main/java/io/underscope/react/fbak/RNAccountKitModule.java
+++ android/src/main/java/io/underscope/react/fbak/RNAccountKitModule.java
@@ -185,7 +185,6 @@ public class RNAccountKitModule extends ReactContextBaseJavaModule implements Ac
      *     initialAuthState             :String
      *     facebookNotificationsEnabled :Boolean
      *     readPhoneStateEnabled        :Boolean
-     *     receiveSMS                   :Boolean
      *     countryBlacklist             :String[]
      *     countryWhitelist             :String[]
      *     defaultCountry               :String
@@ -237,15 +236,6 @@ public class RNAccountKitModule extends ReactContextBaseJavaModule implements Ac
             configurationBuilder.setReadPhoneStateEnabled(false);
         } else {
             configurationBuilder.setReadPhoneStateEnabled(readPhoneStateEnabled);
-        }
-
-        boolean receiveSMS = this.options.getBoolean("receiveSMS");
-        if (receiveSMS && PackageManager.PERMISSION_DENIED == ContextCompat.checkSelfPermission(
-                reactContext.getApplicationContext(), Manifest.permission.RECEIVE_SMS)) {
-            Log.w(REACT_CLASS, "To allow extracting code from SMS add RECEIVE_SMS permission in your app's manifest");
-            configurationBuilder.setReceiveSMS(false);
-        } else {
-            configurationBuilder.setReceiveSMS(receiveSMS);
         }
 
         if (this.options.hasKey("countryBlacklist")) {
